1127011320b13904d8699248dedc0732d370c046
komkit160830-maker <komkit160830@gmail.com>
2026-01-11 20:56:50 +0700

    Now I know

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-## Hello world
-print("Hello world")
-
-# Good bye
-print("Good bye")
-
-## print 1
-print("I'm learning GitHub")
-
-## new function
-def add_two(num):
-  return num+2
-
-print(add_two(2), add_two(5))
+## Now I know
+print("Now I understand github and replit")
